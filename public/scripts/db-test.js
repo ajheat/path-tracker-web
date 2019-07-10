ff3ee421ddd0dfd8fa8edd232c0f807c8b3083d0
Andrew Heatwole <andrewjheatwole@gmail.com>
2019-07-10 18:06:09 -0400

    Updated gitignore and renamed db-test to be ignored

--- public/scripts/db-test.js
+++
@@ -1,7 +0,0 @@
-const db = firebase.firestore();
-
-db.collection('known-items').get().then(function(data) {
-  console.log(data);
-}).catch(function(error) {
-  console.log('Error fetching data: ' + error);
-});
