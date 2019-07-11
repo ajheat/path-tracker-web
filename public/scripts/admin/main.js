9428dd9913f0bf4d6b087bd47a568a1ae323b53a
Andrew Heatwole <andrewjheatwole@gmail.com>
2019-07-10 20:03:12 -0400

    Implemented single case for multi-key items

--- public/scripts/admin/main.js
+++ public/scripts/admin/main.js
@@ -11,14 +11,39 @@ function processFile(event) {
   var list = JSON.parse(event.target.result);
   var collect = db.collection("all-items");
   for (let item of list) {
-    collect.doc(item.name.toLowerCase().replace(/ |\//g,"-")).set({
-      name: item.name,
-      value: item.value.replace(/,|( gp)/g,""),
-      wiki: item.wiki,
-      category: item.category
-    }).then(function() {
-    }).catch(function() {
-      console.log("Failed to write item: " + item.name);
-    });
+    var parts = item.name.split("+");
+    if (parts.length == 1) {
+      collect.doc(itemKey(item.name)).set({
+        name: item.name,
+        value: item.value.replace(/,|( gp)/g,""),
+        wiki: item.wiki,
+        category: item.category
+      }).then(function() {
+      }).catch(function() {
+        console.log("Failed to write item: " + item.name);
+      });
+    } else {
+      console.log(parts);
+      parts[1] = "+" + parts[1];
+      collect.doc(itemKey(parts[0])).set({
+        name: parts[0],
+        wiki: item.wiki,
+        category: item.category
+      }).then(function() {
+      }).catch(function() {
+        console.log("Failed to write item: " + item.name);
+      });
+      var subCollect = db.collection("all-items/" + itemKey(parts[0]) + "/subtypes").doc(parts[1]).set({
+        name: item.name,
+        value: item.value.replace(/,|( gp)/g, ""),
+      }).then(function() {
+      }).catch(function() {
+        console.log("Failed to write item: " + item.name);
+      });
+    } 
   }
+}
+
+function itemKey(itemName) {
+  return itemName.trim().toLowerCase().replace(/ |\//g, "-");
 }
