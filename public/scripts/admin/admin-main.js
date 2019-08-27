e6cac9d3a0bd67a5b088346d61ee4720c79530a4
Andrew Heatwole <andrewjheatwole@gmail.com>
2019-08-26 21:39:45 -0400

    Added resources missed in last commit

---
+++ public/scripts/admin/admin-main.js
@@ -0,0 +1,16 @@
+function handleUpload() {
+  var file = $('#fileInput')[0].files[0]
+  var reader = new FileReader();
+  reader.onload = processFile;
+  reader.readAsText(file);
+  $('#fileInput').val('');
+}
+
+function processFile(event) {
+  var list = JSON.parse(event.target.result);
+  var collect = db.collection("all-items");
+  for (let item of list) {
+    var keyObj = buildKey(item.name);
+    writeItemToDatabase(item, keyObj));
+  }
+}
